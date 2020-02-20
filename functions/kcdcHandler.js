04fb1703bdda1581757099f176b21669e02811b6
PMHStudio <pmhstudio.pmh@gmail.com>
2020-02-20 13:06:56 +0900

    Release v1
    Co-authored-by: TriNet / NoeulNight <ddottybj@naver.com>

---
+++ functions/kcdcHandler.js
@@ -0,0 +1,41 @@
+const path = require('path').resolve()
+const superagent = require('superagent')
+const cheerio = require('cheerio')
+const html2image = require('node-html-to-image')
+let thisNo
+
+exports.newEv = (cb) => {
+  setInterval(() => {
+    superagent.get('https://www.cdc.go.kr/board/board.es?bid=0015')
+      .set('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3945.130 Safari/537.36')
+      .then((res) => {
+        const kcdcBBS = cheerio.load(res.text)
+        if (!thisNo) thisNo = kcdcBBS('.W8')[2].children[0].data
+        else if (thisNo !== kcdcBBS('.W8')[2].children[0].data) {
+          thisNo = kcdcBBS('.W8')[2].children[0].data
+          const obj = { title: '', url: 'https://www.cdc.go.kr', desc: '' }
+          obj.title = kcdcBBS('.ellipsis')[0].children[0].data
+          obj.url += kcdcBBS('.ellipsis')[0].parent.attribs.href
+          superagent.get(obj.url)
+            .set('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3945.130 Safari/537.36')
+            .then((res2) => {
+              const kcdcDesc = cheerio.load(res2.text)
+              kcdcDesc('p').each((i, e) => {
+                if (e.children[0].data) obj.desc += e.children[0].data
+              })
+              obj.desc = obj.desc.slice(0, 100) + '...'
+              if (kcdcDesc('table')) {
+                obj.image = path + '/tables/' + thisNo + '.png'
+                html2image({
+                  html: cheerio.html(kcdcDesc('table').get(0)),
+                  output: path + '/tables/' + thisNo + '.png',
+                  puppeteerArgs: { defaultViewport: { width: 770, height: 250 } }
+                }).then(() => cb(obj))
+              } else {
+                cb(obj)
+              }
+            })
+        }
+      })
+  }, 1000)
+}
---
+++ index.js
@@ -0,0 +1,27 @@
+const { Client, RichEmbed, Attachment } = require('discord.js')
+const kcdc = require('./functions/kcdcHandler')
+const bot = new Client()
+
+bot.login(process.env.kcdcToken)
+bot.on('ready', () => {
+  bot.user.setActivity('질병관리본부', { type: 'WATCHING' })
+  kcdc.newEv((ev) => {
+    const embed = new RichEmbed()
+      .setColor(0xff0000)
+      .setTitle(ev.title)
+      .setURL(ev.url)
+      .setDescription(ev.desc)
+      .setThumbnail('https://pbs.twimg.com/profile_images/839355661607555072/wpQw3vbQ_400x400.jpg')
+
+    bot.channels.forEach((c) => {
+      if (c.name.endsWith('-kcdc')) {
+        bot.channels.get(c.id).send(embed)
+        if (ev.image) bot.channels.get(c.id).send(new Attachment(ev.image))
+      }
+    })
+  })
+})
+
+bot.on('guildCreate', (g) => {
+  g.owner.send('KCDC봇을 초대해 주셔서 감사합니다\n앞으로의 KCDC 소식이 무엇보다도 빠른속도로 전송될것입니다\n주의: **' + g.name + '의 채널중 채널 이름 맨 뒤에 `-kcdc`가 적힌 채널에만 전송됩니다**')
+})
